058d670c3a7c9087d7674d3da23cedfde9b7ce6b
EmilioCorigliano <emilio.corigliano@mail.polimi.it>
2023-02-21 13:54:13 +0100

    [stm32h723zg] Created board stm32h723_nucleo
    - Created board files in arch and config/arch
    - Modified linker script to match flash and ram dimensions
    - Temporarily commented creation of DefaultConsole

--- main.cpp
+++ main.cpp
@@ -7,5 +7,11 @@ using namespace miosix;
 
 int main()
 {
+    while(true){
+        ledOn();
+        delayMs(1000);
+        ledOff();
+        delayMs(1000);
+    }
     //iprintf("Hello world, write your application here\n");
 }
