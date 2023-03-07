7e4e621c8a81d22e8b02954be61dfa9e8a392dc0
EmilioCorigliano <emilio.corigliano@mail.polimi.it>
2023-03-07 13:45:12 +0100

    [stm32h723zg] Working blinking led but wrong delay

--- main.cpp
+++ main.cpp
@@ -5,12 +5,25 @@
 using namespace std;
 using namespace miosix;
 
+typedef Gpio<GPIOB_BASE,14> led_red;
+typedef Gpio<GPIOB_BASE,0> led_green;
+typedef Gpio<GPIOE_BASE,1> led_orange;
+
 int main()
 {
+    
+    led_red::mode(Mode::OUTPUT);
+    led_green::mode(Mode::OUTPUT);
+    led_orange::mode(Mode::OUTPUT);
+
     while(true){
-        ledOn();
+        led_red::high();
+        led_orange::high();
+        led_green::high();
         delayMs(1000);
-        ledOff();
+        led_red::low();
+        led_orange::low();
+        led_green::low();
         delayMs(1000);
     }
     //iprintf("Hello world, write your application here\n");
